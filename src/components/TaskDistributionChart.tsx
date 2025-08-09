a5faa8353d4f000d3b25111412194ade57c8db06
Brahim Allaoui <brahimallaoui1@gmail.com>
2025-08-09 14:38:40 +0000

    Rien n'a changé

--- src/components/TaskDistributionChart.tsx
+++ src/components/TaskDistributionChart.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import * as React from "react"
-import { Pie, PieChart, Cell, Tooltip, ResponsiveContainer, Legend } from "recharts"
+import { Pie, PieChart, Cell, Tooltip, ResponsiveContainer } from "recharts"
 
 import {
   ChartConfig,
