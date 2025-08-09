ec32f9efd7adb04ab7958ae7b5f8b0b0a1a28b16
Brahim Allaoui <brahimallaoui1@gmail.com>
2025-08-09 20:10:45 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "expensetrack-cwxtw",
+  "appId": "1:738703556790:web:e87b7a7745e72468296617",
+  "storageBucket": "expensetrack-cwxtw.firebasestorage.app",
+  "apiKey": "AIzaSyCmCD0dTiNFDT3Kubi_0gTbN2zjJkPyOho",
+  "authDomain": "expensetrack-cwxtw.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "738703556790"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
